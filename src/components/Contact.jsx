f78812f5db864220cf07c129216c63116d53a497
gulshanhasanzade <hasanzade678@gmail.com>
2024-07-23 16:28:26 +0400

    12th commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+import './Contact.css';
 
-export default function Contact() {
+
+const Contact = () => {
   return (
-    <div>Contact</div>
-  )
+   <div div className="dining-us-section">
+    <div className="dining-us-container">
+      <div className="dining-us-background">
+        <div className="dining-us-content">
+          <h1>Contact Us</h1>
+          <nav className="breadcrumb1">
+            <span>Home</span><img src="small.svg" alt="" /><span>Contact Us</span>
+          </nav>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
 }
+
+export default Contact;
+
+
