32e6e276127b8b22891392a2ce768fc5939a6d07
gulshanhasanzade <hasanzade678@gmail.com>
2024-08-05 15:40:36 +0400

    SuccessModal

--- src/components/SuccessMessageModal.jsx
+++ src/components/SuccessMessageModal.jsx
@@ -9,7 +9,7 @@ const SuccessMessageModal = ({ closeModal }) => {
       <div className="success-message-modal">
         <div className="success-message-modal-content">
           <button className="close-button3" onClick={closeModal}>x</button>
-          <img  className="success-icon" src="./group.svg" alt="" />
+          <img  className="success-icon" src="./successIcon.png" alt="" />
           <h2 id='success'>Password Changed Successfully</h2>
           <span>Your password has been updated successfully</span>
           <button id='successBTN' onClick={closeModal}>Back to Login</button>
